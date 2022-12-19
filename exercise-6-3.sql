2f6a13f72bdda453ac430e836e556b4cf737c45c
Ivan-Kislyak <ivangrodno86@gmail.com>
2022-12-19 22:26:20 +0200

    Add exercise-6

---
+++ exercise-6-3.sql
@@ -0,0 +1,11 @@
+﻿SELECT year,
+	COUNT (*)
+	
+FROM album
+WHERE band_id = 388
+	AND year >= 1969
+	AND year <= 1982
+GROUP BY year
+ORDER BY year
+;
+	
---
+++ exercise-6-4.sql
@@ -0,0 +1,9 @@
+﻿SELECT calendar_year.year,
+	album.album_id, album.name,album.band_id,album.year
+FROM calendar_year
+LEFT OUTER JOIN album
+ON calendar_year.year = album.year AND album.band_id = 388
+WHERE calendar_year.year >= 1969 AND calendar_year.year <= 1982
+ORDER BY calendar_year.year
+;
+	
