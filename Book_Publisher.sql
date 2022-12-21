b68a25478ea0632b9eeddfbb2fdf6c3ba61d726c
Ivan-Kislyak <ivangrodno86@gmail.com>
2022-12-21 22:28:12 +0200

    Add Book_Publisher

---
+++ Book_Publisher.sql
@@ -0,0 +1,14 @@
+INSERT INTO book
+VALUES 
+(1, 'The diary of yang girl','01995365566'),
+(2, 'Pride and predigies','9780307594006'),
+(3, 'TO kill a mackinbird','0446310786'),
+(4, 'The book of Gutsy Woman','1501178415'),
+(5, 'War and Peace','1788886526');
+
+INSERT INTO publisher
+VALUES
+(1, 'Evrymens library', 'NY'),
+(2, 'Oxford University', 'NY'),
+(3, 'Grand Central Publishing', 'Washington'),
+(4, 'Simon & Shuster', 'Chicago');
